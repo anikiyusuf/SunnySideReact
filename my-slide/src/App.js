91305504ab2ce69fafe42830986b674db5aa5d2f
Aniki Yusuf <anikiyusuf99@gmail.com>
2022-09-07 15:22:54 +0100

    initial commit

--- my-slide/src/App.js
+++ my-slide/src/App.js
@@ -7,6 +7,11 @@ import Client from "./component/Client"
 import ClientData from './ClientData'
 import ContainerThree from './component/ContainerThree'
 import ContainerFour from './component/ContainerFour'
+import stand1 from './image/image-stand-out-desktop.jpg'
+import stand2 from './image/image-stand-out-mobile.jpg'
+import transform1 from './image/image-transform-desktop.jpg'
+import transform2 from './image/image-transform-mobile.jpg'
+
 
 
 
@@ -25,7 +30,12 @@ const clientElement= ClientData.map(item => {
     return(
         <div className="App">
             <Navbar />
-            <Container />
+            <Container   
+              picture3={transform1} 
+              picture4={transform2}
+              picture1={stand1} 
+              picture2={stand2} 
+          />
             <ContainerTwo/>
             <section className="client-section-all">
                <h1> Client testimonials</h1>
--- my-slide/src/component/Container.js
+++ my-slide/src/component/Container.js
@@ -2,33 +2,35 @@ import React from 'react'
 import "./Container.scss"
 
 
+
 export  default function Container(props){
     return(
-        <div className="container">  
+      <div className="container">  
            <div className="container-section-odd">
            <div className="text-one">
              <h1>Transform your brand</h1>
              <p>Using a collaborative formula of designers, researchers,photographers, videographers, and copywriters, we’ll build and extend your brand in digital places."</p>
              <h3>Learn More</h3>
              </div>
-             <div className="images-one">
-             <img src="image-transform-2.jpg" alt="" className="desktop-image-1" />
-             <img src="image-transform-desktop.jpg"  alt="" className="mobile-image-1" />
+             <div className="image-one">
+             <img src={props.picture3} alt="" className="desktop-image-1" />
+             <img src={props.picture4}  alt="" className="mobile-image-1" />
           </div>
           </div>
 
-      <div className="container-section-even">
-        <div className="text-two">
-          <h1>Stand out to the right audience </h1>
-          <p>Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places."</p>
-          <h3>Learn More </h3>
+        <div className="container-section-even">
+          <div className="text-two">
+            <h1>Stand out to the right audience </h1>
+            <p>Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places."</p>
+            <h3>Learn More </h3>
           </div>
-          <div className="images-two">
-          <img src="image-stand-out.jpg" alt=""  className="mobile-image-2" />
-          <img src="image-stand-out-2.jpg" alt="" className="desktop-image-2" />
+          <div className="image-two">
+          <img src={props.picture1} alt="" className="desktop-image-2" />
+          <img src={props.picture2} alt="sfjfkggskgsbgbj" className="mobile-image-2" />
 
           </div>
         </div>
-        </div>
+
+     </div>
     );
-    }
+   }
